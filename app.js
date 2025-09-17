cfee637f7ee487dee11be99bc02f355d4ac81a10
Jeremy Ang <jang025@users.noreply.github.com>
2025-09-17 17:35:27 +0800

    selection screen functionality + transition to battle screen ✅

--- app.js
+++ app.js
@@ -10,35 +10,49 @@ import {
   salamence,
 } from "./data.js";
 
+const pokemonLookup = {
+  charizard: charizard,
+  blastoise: blastoise,
+  venusaur: venusaur,
+  alakazam: alakazam,
+  machamp: machamp,
+  gengar: gengar,
+  aggron: aggron,
+  salamence: salamence,
+};
+
 /*---------------------------- Variables (state) ----------------------------*/
 
 let playerPokemon;
 let opponentPokemon;
 let playerTurn;
+let selectedPokemonCard = null;
+let selectionPhase = "player";
 
 /*------------------------ Cached Element References ------------------------*/
-// const selectionScreen = document.querySelector(".selection-screen");
-// const battleScreen = document.querySelector(".battle-screen");
-// const resultScreen = document.querySelector(".result-screen");
+const selectionScreen = document.querySelector(".selection-screen");
+const battleScreen = document.querySelector(".battle-screen");
+const resultScreen = document.querySelector(".result-screen");
 
-// const pokemonCards = document.querySelectorAll(".pokemon-card");
-// const confirmBtn = document.querySelector(".btn.confirm");
-// const cancelBtn = document.querySelector(".btn.cancel");
+const selectionTitle = document.querySelector(".selection-title");
+const pokemonCards = document.querySelectorAll(".pokemon-card");
+const confirmBtn = document.querySelector(".btn.confirm");
+const cancelBtn = document.querySelector(".btn.cancel");
 
-// const playerCard = document.querySelector(".card.player");
-// const opponentCard = document.querySelector(".card.opponent");
+const playerCard = document.querySelector(".card.player");
+const opponentCard = document.querySelector(".card.opponent");
 
-// const playerHpFill = document.querySelector(".card.player .hp-fill");
-// const opponentHpFill = document.querySelector(".card.opponent .hp-fill");
-// const playerHpText = document.querySelector(".card.player .hp-text");
-// const opponentHpText = document.querySelector(".card.opponent .hp-text");
+const playerHpFill = document.querySelector(".card.player .hp-fill");
+const opponentHpFill = document.querySelector(".card.opponent .hp-fill");
+const playerHpText = document.querySelector(".card.player .hp-text");
+const opponentHpText = document.querySelector(".card.opponent .hp-text");
 
-// const moveButtons = document.querySelectorAll(".moves .move");
-// const battleLog = document.querySelector(".panel .log");
+const moveButtons = document.querySelectorAll(".moves .move");
+const battleLog = document.querySelector(".panel .log");
 
-// const resultMessage = document.querySelector(".result-message p");
-// const replayBtn = document.querySelector(".result-actions .replay");
-// const newGameBtn = document.querySelector(".result-actions .new-game");
+const resultMessage = document.querySelector(".result-message p");
+const replayBtn = document.querySelector(".result-actions .replay");
+const newGameBtn = document.querySelector(".result-actions .new-game");
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -52,6 +66,57 @@ let playerTurn;
 // This clone is a deep copy, so modifying playerPokemon won’t affect the original pokemon object
 const createPokemon = function (pokemon) {
   return structuredClone(pokemon);
+};
+
+const handlePokemonSelection = function (event) {
+  // select the card
+  const selectedCard = event.currentTarget;
+
+  // remove previous selection --> ensures you cannot select multiple pokemon
+  if (selectedPokemonCard && selectedPokemonCard !== selectedCard) {
+    selectedPokemonCard.classList.remove("selected");
+  }
+
+  // toggle select highlights  when clicking on a card
+  selectedCard.classList.toggle("selected");
+
+  // update the global state of selectedPokemonCard --> track which card is cureently being selected
+  selectedPokemonCard = selectedCard.classList.contains("selected")
+    ? selectedCard
+    : null;
+};
+
+const handleCancelSelection = function () {
+  if (selectedPokemonCard) {
+    // remove selected pokemon
+    selectedPokemonCard.classList.remove("selected");
+    selectedPokemonCard = null;
+  }
+};
+
+const handleConfirmSelection = function () {
+  // no pokemon selected
+  if (!selectedPokemonCard) return;
+  // select the current pokemon card and stores its name
+  const selectedPokemonName =
+    selectedPokemonCard.querySelector(".name").textContent;
+  console.log(selectedPokemonName);
+  if (selectionPhase === "player") {
+    playerPokemon = createPokemon(pokemonLookup[selectedPokemonName]);
+    console.log(playerPokemon);
+    selectionTitle.textContent = "Select Opponent Pokémon";
+    selectionPhase = "opponent";
+
+    // clear selection after confirming
+    selectedPokemonCard.classList.remove("selected");
+    selectedPokemonCard = null;
+  } else if (selectionPhase === "opponent") {
+    opponentPokemon = createPokemon(pokemonLookup[selectedPokemonName]);
+    console.log(opponentPokemon);
+    // transition the screen back to battle screen
+    selectionScreen.classList.add("hide");
+    battleScreen.classList.remove("hide");
+  }
 };
 
 const calculateDamage = function (attacker, defender, move) {
@@ -87,46 +152,48 @@ const logStatus = function () {
   );
 };
 
-/*------------------------------ Game setup and battle loop -----------------*/
-playerPokemon = createPokemon(charizard);
-console.log(playerPokemon);
-opponentPokemon = createPokemon(salamence);
-console.log(opponentPokemon);
-
-// Decide who goes first
-playerTurn = playerPokemon.speed >= opponentPokemon.speed;
-console.log(playerTurn);
-
-console.log(`Player chose ${playerPokemon.name}!`);
-console.log(`Opponent chose ${opponentPokemon.name}!`);
-console.log(playerTurn ? "Player goes first!" : "Opponent goes first!");
-
-// Main battle loop
-while (playerPokemon.hp > 0 && opponentPokemon.hp > 0) {
-  if (playerTurn) {
-    // hard code the first move for now
-    const { damage, move } = applyMove(playerPokemon, opponentPokemon, 0);
-    console.log(`Player used : ${move.name} and damage dealt : ${damage}`);
-  } else {
-    const { damage, move } = applyMove(opponentPokemon, playerPokemon, 0);
-    console.log(`Opponent used : ${move.name} and damage dealt : ${damage}`);
-  }
-
-  // battle logs
-  logStatus();
-
-  // switch turns
-  playerTurn = !playerTurn;
-}
-
-// battle outcome
-if (playerPokemon.hp > 0) {
-  console.log("Player wins!");
-} else {
-  console.log("Opponent wins!");
-}
-
 /*----------------------------- Event Listeners -----------------------------*/
+
+for (let i = 0; i < pokemonCards.length; i++) {
+  pokemonCards[i].addEventListener("click", handlePokemonSelection);
+}
+confirmBtn.addEventListener("click", handleConfirmSelection);
+cancelBtn.addEventListener("click", handleCancelSelection);
+
+/*------------------------------ Game setup and battle loop -----------------*/
+
+// // Decide who goes first
+// playerTurn = playerPokemon.speed >= opponentPokemon.speed;
+// console.log(playerTurn);
+
+// console.log(`Player chose ${playerPokemon.name}!`);
+// console.log(`Opponent chose ${opponentPokemon.name}!`);
+// console.log(playerTurn ? "Player goes first!" : "Opponent goes first!");
+
+// // Main battle loop
+// while (playerPokemon.hp > 0 && opponentPokemon.hp > 0) {
+//   if (playerTurn) {
+//     // hard code the first move for now
+//     const { damage, move } = applyMove(playerPokemon, opponentPokemon, 0);
+//     console.log(`Player used : ${move.name} and damage dealt : ${damage}`);
+//   } else {
+//     const { damage, move } = applyMove(opponentPokemon, playerPokemon, 0);
+//     console.log(`Opponent used : ${move.name} and damage dealt : ${damage}`);
+//   }
+
+//   // battle logs
+//   logStatus();
+
+//   // switch turns
+//   playerTurn = !playerTurn;
+// }
+
+// // battle outcome
+// if (playerPokemon.hp > 0) {
+//   console.log("Player wins!");
+// } else {
+//   console.log("Opponent wins!");
+// }
 
 //! things to do next
 
@@ -138,7 +205,6 @@ if (playerPokemon.hp > 0) {
 //todo -->  Show battle log messages for each move. Update UI dynamically
 // todo -->  Show a “disabled” state for moves with usage <= 0. Update UI dynamically
 // todo --> trigger a result screen when either Pokémon HP ≤ 0. Determine win / loss
-// todo --> animation for attacks (UI / UX enhancement)
 
 //todo --> edge cases : all 4 moves has zero usage left
 
